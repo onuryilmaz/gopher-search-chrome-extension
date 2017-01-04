1e1f32d073b55afe5973a70ebd565ff414e307c1
Onur Yilmaz <onur.yilmaz@sap.com>
2017-01-05 00:04:09 +0300

    first commit

---
+++ background.js
@@ -0,0 +1,26 @@
+chrome.browserAction.onClicked.addListener(function(tab) {
+
+    chrome.storage.local.get("active", function(item) {
+
+        if (item.active == "true") {
+            chrome.storage.local.set({
+                "active": "false"
+            }, function() {
+                chrome.browserAction.setIcon({
+                    path: "go-fade.png"
+                });
+
+            });
+
+        } else {
+            chrome.storage.local.set({
+                "active": "true"
+            }, function() {
+                chrome.browserAction.setIcon({
+                    path: "gopher.png"
+                });
+
+            })
+        }
+    });
+});
---
+++ content.js
@@ -0,0 +1,22 @@
+const becomeGopher = function() {
+    chrome.storage.local.get("active", function(item) {
+
+        if (item.active != "false") {
+            const searchbox = [...document.getElementsByName('q')];            
+            const reg = new RegExp("\\b" + "go" + "\\b");
+            searchbox.forEach(function(sb) {
+                if (reg.test(sb.value) && !sb.value.includes("golang")) {
+                    console.log("found!")
+                    sb.value = sb.value.replace(reg, "golang");
+                    document.getElementsByClassName('tsf').tsf.submit();
+                }
+            });
+
+        }
+    });
+
+}
+
+$('input[name=q]').change(function() {becomeGopher(); });
+
+becomeGopher();
